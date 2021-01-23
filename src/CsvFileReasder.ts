5b57e2cd555698c137c96fc3b20ac97e5c9b377d
mpilopillz <mpilo.dlamini@yahoo.com>
2021-01-23 11:23:29 +0200

    adding tuples for cleaner return type syntax

--- src/CsvFileReasder.ts
+++ src/CsvFileReasder.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-    data: string[][] = [];
+    data: MatchData[] = [];
     
     constructor(public filename: string) { }
 
@@ -13,7 +15,7 @@ export class CsvFileReader {
         }).split('\n')
         .map((row: string): string[] => {
             return row.split(',')
-        }).map((row: string[]): (Date | string | number | MatchResult) [] => {
+        }).map((row: string[]): MatchData => {
             return [
                 dateStringToDate(row[0]),
                 row[1],
