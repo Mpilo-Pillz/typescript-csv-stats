dcbbb8c217737912f0ea2a7eee62047b39dcf447
mpilopillz <mpilo.dlamini@yahoo.com>
2021-01-23 05:41:39 +0200

    first commit with hardcoded values

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+    encoding: 'utf-8'
+}).split('\n').map((row: string): string[] => {
+    return row.split(',')
+})
+
+let manCityWins = 0;
+
+for (let match of matches) {
+    if(match[1] === 'Man City' && match[5] === 'H') {
+        manCityWins++;
+    } else if (match[2] === 'Man City' && match[5] === 'A') {
+        manCityWins++;
+    }
+}
+
+console.log(`Man City won ${manCityWins} games`);
+
+
