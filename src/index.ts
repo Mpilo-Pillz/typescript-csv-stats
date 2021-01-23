571a73348bcd025ab65faf6af17c32be8324eba8
mpilopillz <mpilo.dlamini@yahoo.com>
2021-01-23 21:09:05 +0200

    alternative refactor with nterfaces

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult'
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of Match Reader and pass in something satisfting
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
 
 
 
 let manCityWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if(match[1] === 'Man City' && match[5] === MatchResult.HomeWin) {
         manCityWins++;
     } else if (match[2] === 'Man City' && match[5] === MatchResult.AwayWin) {
