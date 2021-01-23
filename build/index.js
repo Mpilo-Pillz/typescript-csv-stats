5924388b260d2a193f22a2ae013cde56aa4f9dca
mpilopillz <mpilo.dlamini@yahoo.com>
2021-01-23 06:10:44 +0200

    adding notes and abstracting csv file reader for reusablility

--- build/index.js
+++ build/index.js
@@ -1,14 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map(function (row) {
-    return row.split(',');
-});
+var CSVFileReasder_1 = require("./CSVFileReasder");
+var reader = new CSVFileReasder_1.CsvFileReader('football.csv');
+reader.read();
 var MatchResult;
 (function (MatchResult) {
     MatchResult["HomeWin"] = "H";
@@ -17,8 +11,8 @@ var MatchResult;
 })(MatchResult || (MatchResult = {}));
 ;
 var manCityWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
     if (match[1] === 'Man City' && match[5] === MatchResult.HomeWin) {
         manCityWins++;
     }
